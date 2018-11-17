d05903d532264ff6c25e7e1f5ae86edcc69d0ecc
hyunjun <homelus@daum.net>
2018-11-17 16:49:28 +0900

    [6.8.2] 트랜잭션 동기화 테스트 코드 추가

--- src/test/java/v6/UserServiceTest.java
+++ src/test/java/v6/UserServiceTest.java
@@ -19,6 +19,8 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -167,6 +169,51 @@ public class UserServiceTest {
     @Test
     public void readOnlyTransactionAttribute() {
         testUserService.getAll();
+    }
+
+    @Test
+    public void transactionSync() {
+        DefaultTransactionDefinition txDefinition = new DefaultTransactionDefinition();
+        txDefinition.setReadOnly(true);
+        TransactionStatus txStatus = transactionManager.getTransaction(txDefinition);
+
+        userService.deleteAll();
+
+        userService.add(users.get(0));
+        userService.add(users.get(1));
+
+        transactionManager.commit(txStatus);
+    }
+
+    @Test
+    public void tranactionSyncRollback() {
+        userDao.deleteAll();
+        assertThat(userDao.getCount(), is(0));
+
+        DefaultTransactionDefinition txDefinition = new DefaultTransactionDefinition();
+        TransactionStatus txStatus = transactionManager.getTransaction(txDefinition);
+
+        userService.add(users.get(0));
+        userService.add(users.get(1));
+        assertThat(userDao.getCount(), is(2));
+
+        transactionManager.rollback(txStatus);
+        assertThat(userDao.getCount(), is(0));
+    }
+
+    @Test
+    public void transactionRollback() {
+        DefaultTransactionDefinition txDefinition = new DefaultTransactionDefinition();
+        TransactionStatus txStatus = transactionManager.getTransaction(txDefinition);
+
+        try {
+            userService.deleteAll();
+            userService.add(users.get(0));
+            userService.add(users.get(1));
+        }
+        finally {
+            transactionManager.rollback(txStatus);
+        }
     }
 
     private void checkUserAndLevel(User updated, String expectedId, Level expectedLevel) {
